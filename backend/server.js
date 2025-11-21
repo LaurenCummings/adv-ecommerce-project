ea40aa121128bf18d06a6897a8d5c8e4f671f3cc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-21 03:19:41 -0500

    console logged port to test out .env file

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+console.log(process.env.PORT);
+// const PORT = 5000;
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
