8fb56f9c31f63f466041ea94407f4800782d7112
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-21 03:21:59 -0500

    updated 'server running on' printout to be more accurate

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
