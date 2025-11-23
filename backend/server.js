90e7a5ee0d9236d1dbcb136e6df284066f0fa775
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-22 23:22:29 -0500

    set default port to be 5000 if no port is set in environment variables

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
