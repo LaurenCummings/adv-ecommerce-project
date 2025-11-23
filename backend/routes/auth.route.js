58c6911ea49ad1bc1594f4455bfff3dd3911f4cd
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-22 23:48:57 -0500

    created routes for login and logout

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 router.get("/signup", (req, res) => {
     res.send("Sign up route called");
-})
+});
+
+router.get("/login", (req, res) => {
+    res.send("Login route called");
+});
+
+router.get("/logout", (req, res) => {
+    res.send("Logout route called");
+});
 
 export default router;
