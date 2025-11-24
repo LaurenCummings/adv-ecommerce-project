f486c0ecec7e77ae4da751dcc842fd1a1ecb54ef
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-24 03:44:20 -0500

    Added status code and message if user is created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -8,6 +8,8 @@ export const signup = async (req, res) => {
         return res.status(400).json({ message: "User already exists" });
     }
     const user = await User.create({ name, email, password });
+
+    res.status(201).json({ user, message: "User created successfully" });
 };
 
 export const login = async (req, res) => {
