4c676a51b3ab647a57399b914ad58d857ff94db5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-24 03:40:36 -0500

    called function to check if user exists by their email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
+    const userExists = await User.findOne({ email });
     
     res.send("Sign up route called");
 };
