472291b77fe9718e2d8065c09e65e61bdab68eaa
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-24 03:42:45 -0500

    if user doesn't exist, created new user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -7,6 +7,7 @@ export const signup = async (req, res) => {
     if (userExists) {
         return res.status(400).json({ message: "User already exists" });
     }
+    const user = await User.create({ name, email, password });
 };
 
 export const login = async (req, res) => {
