c2e24248c08b147c5efc365fa97c2fec9af5b0e1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-25 04:06:49 -0500

    created redis instance using code from redis docs

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,4 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+
