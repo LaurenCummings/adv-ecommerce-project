d5770f11e8076942e27827305691d51d9eac3318
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-27 00:15:49 -0500

    finished setting refresh token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -23,7 +23,13 @@ const setCookies = (res, accessToken, refreshToken) => {
         httpOnly: true, // prevent XSS attacks
         secure: process.env.NODE_ENV === "production", 
         sameSite: "strict", // prevents CSRF attack
-        maxAge: 15 * 60 * 1000
+        maxAge: 15 * 60 * 1000, // 15 minutes
+    })
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, // prevent XSS attacks
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: "strict", // prevents CSRF attack
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
 }
 
