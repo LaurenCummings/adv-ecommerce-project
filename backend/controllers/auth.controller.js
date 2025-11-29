5e219cc9d653395c4513dee597759f1eb840ccb6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-11-29 01:27:51 -0500

    grabbed email and password for login function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,7 +65,7 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     try {
-
+        const { email, password } = req.body;
     } catch (error) {
         
     }
