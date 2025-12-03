6a61db9fd73bc6443613a57c7884f33cfe50cbac
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-02 22:34:43 -0500

    if user is found, set the user in the request to be found user and called the next function

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -13,8 +13,13 @@ export const protectRoute = async (req, res, next) => {
         const user = await User.findById(decoded.userId).select("-password");
 
         if (!user) {
-            return res.status(401).json({ message: "Unauthorized - Invalid access token" });
+            return res.status(401).json({ message: "User not found" });
         }
+
+        req.user = user;
+
+        next();
+        
     } catch (error) {
 
     }
