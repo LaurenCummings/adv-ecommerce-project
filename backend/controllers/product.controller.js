d80f5fc653557c35f93bec042d3018a5d9230967
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-02 22:20:52 -0500

    return status code and message if there's an error

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -6,5 +6,6 @@ export const getAllProducts = async (req, res) => {
         res.json({ products });
     } catch (error) {
         console.log("Error in getAllProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
     }
 };
