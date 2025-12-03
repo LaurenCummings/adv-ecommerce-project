ae5f0b6f0a39e2b031a8c60a8d21035252336f30
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-02 22:33:24 -0500

    returned status code and message if no user is found using access token

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -11,6 +11,10 @@ export const protectRoute = async (req, res, next) => {
 
         const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized - Invalid access token" });
+        }
     } catch (error) {
 
     }
