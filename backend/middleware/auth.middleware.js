21f71419b9ae91213d9a9496576aeaddaf1de906
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-04 03:26:16 -0500

    changed unauthorized return message slightly

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -30,6 +30,6 @@ export const adminRoute = (req, res, next) => {
     if (req.user && req.user.role === "admin") {
         next();
     } else {
-        return res.status(403).json({ message: "Unauthorized - Admin only" });
+        return res.status(403).json({ message: "Access denied - Admin only" });
     }
 };
