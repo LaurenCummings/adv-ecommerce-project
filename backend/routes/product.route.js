0c25a2c6a51f42d4b5ab5d5d12263d5b2ed8f998
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-05 01:56:50 -0500

    imported getFeaturedProducts into product route file

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
