3b51170b062c1e473a5ce2c85bcc4545d29b35dc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-08 02:54:30 -0500

    return status code and message if product isn't found

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -66,6 +66,10 @@ export const createProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
     } catch (error) {
 
     }
