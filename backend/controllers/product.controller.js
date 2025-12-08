b931ce563c141f348ed624723d7db7e89fb7d228
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-08 01:58:26 -0500

    created cloudinaryResponse variable

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -40,6 +40,8 @@ export const getFeaturedProducts = async (req, res) => {
 export const createProduct = async (req, res) => {
     try {
         const { name, description, price, image, caterogy } = req.body;
+
+        let cloudinaryResponse = null;
     } catch (error) {
 
     }
