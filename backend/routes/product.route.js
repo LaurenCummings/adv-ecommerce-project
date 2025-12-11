c437bee1929ab8574b17c4d4fd96096522755884
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-11 02:39:02 -0500

    added getProductsByCategory route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, getAllProducts, createProduct);
 router.post("/:id", protectRoute, adminRoute, deleteProduct);
