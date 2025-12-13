0c96df146025824746eb1ba21ef4ed520e87091b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-12 22:08:18 -0500

    added toggleFeaturedProduct route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -9,6 +9,7 @@ router.get("/featured", getFeaturedProducts);
 router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, getAllProducts, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
 router.post("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
