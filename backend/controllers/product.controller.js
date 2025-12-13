61aaacfabc857563f52711a6de0f43f9f2a2e6c1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-12 22:57:58 -0500

    fixed typo

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -128,7 +128,7 @@ export const toggleFeaturedProduct = async (req, res) => {
         if (product) {
             product.isFeatured = !product.isFeatured;
             const updatedProduct = await product.save();
-            // await updateFeaturedProductCache();
+            // await updateFeaturedProductsCache();
             res.json(updatedProduct);
         } else {
             res.status(404).json({ message: "Product not found" });
@@ -139,6 +139,6 @@ export const toggleFeaturedProduct = async (req, res) => {
     }
 };
 
-async function updateFeaturedProductCache() {
+async function updateFeaturedProductsCache() {
 
 };
