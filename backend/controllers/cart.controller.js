062a978ef2af0ad077bed8434f4c3ae60eeb6a7c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-15 01:48:54 -0500

    added getCartProducts function to cart controller

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -3,3 +3,5 @@ export const addToCart = async (req, res) => {};
 export const removeAllFromCart = async (req, res) => {};
 
 export const updateQuantity = async (req, res) => {};
+
+export const getCartProducts = async (req, res) => {};
