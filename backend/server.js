fe3ea1fabb5df9e501b65f07c07e6d93c6e5e1f4
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-18 23:57:24 -0500

    added coupon routes using middleware

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
