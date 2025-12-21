f250359f6ed3f81fa2008d06c643ebeca2ed92f6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-21 17:05:46 -0500

    if there's an error, logged it to the console

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -5,6 +5,6 @@ export const getCoupon = async (req, res) => {
         const coupon = await Coupon.findOne({ userId: req.user._id, isActive: true });
         res.json(coupon || null);
     } catch (error) {
-        
+        console.log("Error in getCoupon controller", error.message);
     }
 };
