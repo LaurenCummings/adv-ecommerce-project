783818ea7b26f86aa0413863167c616f0253da29
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-21 17:10:15 -0500

    added validate coupon route to coupon routes

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -5,5 +5,6 @@ import { getCoupon, validateCoupon } from "../controllers/coupon.controller.js";
 const router = express.Router();
 
 router.get("/", protectRoute, getCoupon);
+router.get("/validate", protectRoute, validateCoupon);
 
 export default router;
