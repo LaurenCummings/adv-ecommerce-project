d474d0c1b315588b1fee39002ec63655fbc4ebb7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-21 17:04:22 -0500

    returned either the coupon or null (if there isn't a coupon) in the response

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -3,6 +3,7 @@ import Coupon from "../models/coupon.model.js";
 export const getCoupon = async (req, res) => {
     try {
         const coupon = await Coupon.findOne({ userId: req.user._id, isActive: true });
+        res.json(coupon || null);
     } catch (error) {
         
     }
