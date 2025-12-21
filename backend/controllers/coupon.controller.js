4171aba12a156b6e510c2461e5b8c957c400bcf6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-21 17:03:43 -0500

    performed search using userId to find one coupon that is active

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -2,7 +2,7 @@ import Coupon from "../models/coupon.model.js";
 
 export const getCoupon = async (req, res) => {
     try {
-        
+        const coupon = await Coupon.findOne({ userId: req.user._id, isActive: true });
     } catch (error) {
         
     }
