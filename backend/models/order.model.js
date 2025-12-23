26e4ce2b647a212d5d5f8613d65863dabedca1c3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-23 00:29:06 -0500

    added user to order schema

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
         totalAmount: {
             type: Number,
             required: true,
