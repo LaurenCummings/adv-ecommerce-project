75fd3f710dd7ae661e674dcdabff9f4efd20c45f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-24 02:57:05 -0500

    added protectRoute to createCheckoutSession route

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -3,6 +3,6 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/create-checkout-session");
+router.post("/create-checkout-session", protectRoute);
 
 export default router;
