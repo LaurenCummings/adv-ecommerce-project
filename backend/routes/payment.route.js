8a234d8c0a3224d58176cd292e86b4ce0e900465
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-28 03:16:02 -0500

    added checkoutSuccess route to payment routes

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -5,5 +5,6 @@ import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.c
 const router = express.Router();
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, checkoutSuccess);
 
 export default router;
