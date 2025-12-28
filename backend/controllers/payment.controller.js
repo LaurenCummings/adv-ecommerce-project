c24e76c2b672dd7f4e6ed098334a7ba1e240ae31
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-28 03:25:15 -0500

    added products with id, quantity, and price to metadata

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -51,8 +51,15 @@ export const createCheckoutSession = async (req, res) => {
                     : [],
                 metadata: {
                     userId: req.user._id.toString(),
-                    couponCode: couponCode || ""
-                }
+                    couponCode: couponCode || "",
+                    products: JSON.stringify(
+                        products.map((p) => ({
+                            id: p._id,
+                            quantity: p.quantity,
+                            price: p.price,
+                        }))
+                    ),
+                },
             });
 
             if (totalAmount >= 20000) {
