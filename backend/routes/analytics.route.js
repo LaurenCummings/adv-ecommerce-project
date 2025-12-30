2492b520d1ea4097669c326458a8ef001f940e3b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-30 01:02:14 -0500

    added getAnalyticsData route to analytics routes

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -4,4 +4,6 @@ import { getAnalyticsData } from "../controllers/analytics.controller.js";
 
 const router = express.Router();
 
+router.get("/", protectRoute, adminRoute, getAnalyticsData);
+
 export default router;
