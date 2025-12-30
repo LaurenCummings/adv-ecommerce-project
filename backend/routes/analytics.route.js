5f9263aa6d2c0316e59a7b226e859c17ef790bd8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-30 01:05:05 -0500

    added analyticsData constant to getAnalyticsData route

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, async (req, res) => {
     try {
-
+        const analyticsData = await getAnalyticsData();
     } catch (error) {
-        
+
     }
 });
 
