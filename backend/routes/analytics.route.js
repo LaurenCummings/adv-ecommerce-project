440bb064c712bd1b0b1bbf2f3c8bf51f8fabcdf8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-30 00:58:56 -0500

    added js extension to import

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { adminRoute, protectRoute } from "../middleware/auth.middleware";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
