b33d8db2014ab89c1c75ee78fb80220051473af8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-30 01:04:33 -0500

    changed the structure of the getAnalyticsData route

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -4,6 +4,12 @@ import { getAnalyticsData } from "../controllers/analytics.controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, adminRoute, getAnalyticsData);
+router.get("/", protectRoute, adminRoute, async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
 
 export default router;
