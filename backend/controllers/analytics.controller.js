96497dcd97e95f55a0454bb869e242e844560d3a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-12-30 01:08:34 -0500

    imported Product model into analytics controller

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,4 +1,5 @@
 import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
 import User from "../models/user.model.js";
 
 export const getAnalyticsData = async (req, res) => {
