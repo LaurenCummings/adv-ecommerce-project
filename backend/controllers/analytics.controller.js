17cb68f515d08955c832c235c3a27590fd5cce20
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-01 02:30:25 -0500

    put getDailySalesData inside a try/catch block

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -27,36 +27,40 @@ export const getAnalyticsData = async (req, res) => {
 };
 
 export const getDailySalesData = async (startDate, endDate) => {
-    const dailySalesData = await Order.aggregate([
-        {
-            $match: {
-                createdAt: {
-                    $gte: startDate,
-                    $lte: endDate,
+    try {
+        const dailySalesData = await Order.aggregate([
+            {
+                $match: {
+                    createdAt: {
+                        $gte: startDate,
+                        $lte: endDate,
+                    },
                 },
             },
-        },
-        {
-            $group: {
-                _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
-                sales: { $sum: 1 },
-                revenue: { $sum: "$totalAmount" },
+            {
+                $group: {
+                    _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                    sales: { $sum: 1 },
+                    revenue: { $sum: "$totalAmount" },
+                },
             },
-        },
-        { $sort: { _id: 1 } },
-    ]);
+            { $sort: { _id: 1 } },
+        ]);
 
-    const dateArray = getDatesInRange(startDate, endDate);
+        const dateArray = getDatesInRange(startDate, endDate);
 
-    return dateArray.map(date => {
-        const foundData = dailySalesData.find(item => item._id === date);
+        return dateArray.map(date => {
+            const foundData = dailySalesData.find(item => item._id === date);
 
-        return {
-            date,
-            sales: foundData?.sales || 0,
-            revenue: foundData?.revenue || 0,
-        }
-    })
+            return {
+                date,
+                sales: foundData?.sales || 0,
+                revenue: foundData?.revenue || 0,
+            }
+        })
+    } catch (error) {
+
+    }
 };
 
 function getDatesInRange(startDate, endDate) {
