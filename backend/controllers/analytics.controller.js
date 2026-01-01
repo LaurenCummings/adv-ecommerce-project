cf8ee4d558b1b731d0fc41343a164217089d062b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-01 02:21:36 -0500

    wrote a function to return an array of dates between a specified start date and end date

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -49,5 +49,13 @@ export const getDailySalesData = async (startDate, endDate) => {
 };
 
 function getDatesInRange(startDate, endDate) {
-    
+    const dates = [];
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= endDate) {
+        dates.push(currentDate.toISOString().split("T")[0]);
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dates;
 }
