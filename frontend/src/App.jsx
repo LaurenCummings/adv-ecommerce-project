a5b09b3dc26dd8ae9c4ba5aad1ce5623a9232e91
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-04 02:54:09 -0500

    uninstalled packages from root directory- moving to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+// import { Route, Routes } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <Routes>
+    <div> APp
+      {/* <Routes>
         <Route path="/" element={ <HomePage /> } />
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
