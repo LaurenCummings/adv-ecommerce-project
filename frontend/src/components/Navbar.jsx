d41ce4ebda7db9d418ba58f930a12a21d655958c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-05 03:18:41 -0500

    added styling to navbar header

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header>
+    <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
 
     </header>
   )
