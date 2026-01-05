0087e887d0c8f174d2e3ed12218bc6d5e008343c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-05 02:45:51 -0500

    imported signUpPage into app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
