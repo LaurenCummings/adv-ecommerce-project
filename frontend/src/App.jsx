1f9cea4bb5d804d41fbfd323ba270eb4c312022c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-05 02:41:14 -0500

    deleted signUp page route for now due to error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div> APp
       <Routes>
         <Route path="/" element={ <HomePage /> } />
-        <Route path="/signup" element={ <SignUpPage /> } />
         <Route path="/login" element={ <LoginPage /> } />
       </Routes>
     </div>
