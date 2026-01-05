735433ae397d0abce400b93837bf6179b1d60a6c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-05 02:50:24 -0500

    added classnames to app div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div> 
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden"> 
       <Navbar />
       <Routes>
         <Route path="/" element={ <HomePage /> } />
