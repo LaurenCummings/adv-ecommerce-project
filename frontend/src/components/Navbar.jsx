87dde52cf4ed53ae82cb2ab4621b673a6493aa42
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-09 00:25:15 -0500

    added styling to cart link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
             <nav className="flex flex-wrap items-center gap-4">
               <Link to={"/"} className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out">Home</Link>
               { user && (
-                <Link to={"/cart"} className="relative group">
+                <Link to={"/cart"} className="relative group text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out">
                   <ShoppingCart className="inline-block mr-1 group-hover:text-emerald-400" size={20} />
                   <span className="hidden sm:inline">Cart</span>
                   <span className="absolute -top-2 -right-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">3</span>
