aa368841a35a4d2db5a01f2015bec0ab94be151c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-09 00:01:13 -0500

    added styling to shopping cart quantity

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
                 <Link to={"/cart"} className="relative group">
                   <ShoppingCart className="inline-block mr-1 group-hover:text-emerald-400" size={20} />
                   <span className="hidden sm:inline">Cart</span>
-                  <span>3</span>
+                  <span className="absolute -top-2 -right-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">3</span>
                 </Link>
               )}
             </nav>
