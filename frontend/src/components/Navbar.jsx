b81a6d122d7cd5485cd8de889af2194546f4a4fd
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-09 00:25:42 -0500

    added user constant

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const user = true;
+
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
         <div className="container mx-auto px-4 py-3">
