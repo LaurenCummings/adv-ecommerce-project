d56238dc43c485b74d4c0f0aaa58b51b58bb4adc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-10 23:55:15 -0500

    added styling to lock icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
               )}
               { isAdmin && (
                 <Link className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium transition duration-300 ease-in-out flex items-center">
-                  <Lock />
+                  <Lock className="inline-block mr-1" size={18} />
                 </Link>
               )}
             </nav>
