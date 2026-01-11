41e76c627e36f1a83a686185f543f308ddf825b5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-11 00:09:10 -0500

    added icon to signup link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,6 +36,7 @@ function Navbar() {
               ) : (
                 <>
                   <Link to={"/signup"}>
+                    <UserPlus />
                   </Link>
                 </>
               )}
