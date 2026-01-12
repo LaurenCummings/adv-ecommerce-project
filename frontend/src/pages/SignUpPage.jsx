19e4cc99611359389d435b6543a104017d33aa80
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-12 01:49:48 -0500

    added loading constant

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -4,6 +4,8 @@ import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
 
 function SignUpPage() {
+  const loading = true;
+  
   return (
     <div>SignUpPage</div>
   )
