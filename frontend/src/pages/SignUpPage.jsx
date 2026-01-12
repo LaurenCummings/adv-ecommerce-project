16b12231459b7f0d294d2a646708e9138d8ca51e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-12 01:23:02 -0500

    imported motion for animations into signUpPage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import { motion } from "framer-motion";
 
 function SignUpPage() {
   return (
