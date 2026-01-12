a7c48c2f2652c2670cec4b5f4e3844e4242768ba
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-12 01:22:19 -0500

    imported icons into signupPage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 
 function SignUpPage() {
   return (
