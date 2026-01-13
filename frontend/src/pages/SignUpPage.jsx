eda3e341da74b5ffe4cb87d7d26b9f17c378f580
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-13 01:06:40 -0500

    added div inside second motion.div

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -33,7 +33,9 @@ function SignUpPage() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-
+        <div>
+          
+        </div>
       </motion.div>
     </div>
   )
