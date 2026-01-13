73cb7187f6414c59a4bef7b16dffe3b21f77c79b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-13 00:54:22 -0500

    altered the animation properties slightly

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -20,9 +20,9 @@ function SignUpPage() {
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <motion.div
-        initial={{ opacity: 0, y:20 }}
-        animate={{ opacity:1, y:0 }}
-        transition={{ duration: 0.8, delay: 0.2 }}
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
       >
         <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">Create your Account</h2>
       </motion.div>
