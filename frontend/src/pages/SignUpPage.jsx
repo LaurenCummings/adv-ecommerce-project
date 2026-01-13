f8c835c1da5dd628ae1b0eb3f93598c56482f729
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-13 01:06:15 -0500

    added animation properties to second motion.div

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -29,6 +29,9 @@ function SignUpPage() {
 
       <motion.div
         className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
       >
 
       </motion.div>
