3c07d34e54ebc36c08c09e90050e44e33ecb6cc7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-17 00:57:12 -0500

    added loading constant to logInPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,6 +6,7 @@ import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
 function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const loading = true;
 
   return (
     <div>LoginPage</div>
