9589ad65b07822fd4e5b26d5b9556468a4dd0403
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-17 00:54:43 -0500

    imported icons into LogIn page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
 
 function LoginPage() {
   return (
