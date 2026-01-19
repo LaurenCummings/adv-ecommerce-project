02dd1715b3e08782f0a45cd264bacaa2ebd766c9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-18 23:43:44 -0500

    changed icon on login button

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -79,7 +79,7 @@ function LoginPage() {
                 </>
               ) : (
                 <>
-                  <UserPlus className="mr-2 h-5 w-5" aria-hidden="true" />
+                  <LogIn className="mr-2 h-5 w-5" aria-hidden="true" />
                   Login
                 </>
               )}
