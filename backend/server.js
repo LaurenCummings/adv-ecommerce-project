b87c8eb2373f4c2d900a9cdaf306b30981e2828b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-21 00:54:01 -0500

    deleted cors reference

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -19,12 +18,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
 
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes);
