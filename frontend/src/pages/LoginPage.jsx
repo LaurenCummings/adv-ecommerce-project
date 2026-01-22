01fceabff4ca0e1ac7c6c9eefb524795afb16c88
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-22 17:36:17 -0500

    imported useUserStore into loginPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
@@ -11,6 +12,7 @@ function LoginPage() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password);
+    
   }
 
   return (
