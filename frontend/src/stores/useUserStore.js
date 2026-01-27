f0a18ed035018ad27f55aa094291dc243397dceb
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-26 23:55:50 -0500

    stored response from axios call in user variable and set checkingAUth to false

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -40,6 +40,7 @@ export const useUserStore = create((set, get) => ({
         set({ checkingAuth: true });
         try {
             const response = await axios.get("/auth/profile");
+            set({ user: response.data, checkingAuth: false });
         } catch (error) {
 
         }
