6587404c2e4c7954d8f68a0c36b6932f9b9ce80d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-29 00:01:39 -0500

    added statement to show LoadingSpinner if checkingAuth is true

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     checkAuth();
   }, [checkAuth]);
 
+  if (checkingAuth) return <LoadingSpinner />;
 
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden"> 
