77e19149b019d5510dc056cd53a10466634b5fd1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-29 01:15:23 -0500

    set user to null in logout function

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -39,6 +39,7 @@ export const useUserStore = create((set, get) => ({
     logout: async () => {
         try {
             await axios.post("/auth/logout");
+            set({ user: null });
         } catch (error) {
 
         }
