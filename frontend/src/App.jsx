af1983139efd1a7fdfc08650296c2bf3fb4eb131
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-28 23:49:58 -0500

    added conditional to signup route to navigate user to home page if they've already logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={ <HomePage /> } />
-          <Route path="/signup" element={ <SignUpPage /> } />
+          <Route path="/signup" element={ !user ? <SignUpPage /> : <Navigate to="/" /> } />
           <Route path="/login" element={ !user ? <LoginPage /> : <Navigate to="/" /> } />
         </Routes>      
       </div>
