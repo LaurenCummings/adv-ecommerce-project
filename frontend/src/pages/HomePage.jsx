fbde4cdcde6e6137fcf3cefad239288f77639a2f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-30 02:49:29 -0500

    updated hrefs for other categories

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 const categories = [
   { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/tshirts", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/shoes", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/glasses", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/jackets", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/suits", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/bags", name: "Jeans", imageUrl: "/jeans.jpg" },
 ];
 
 function HomePage() {
