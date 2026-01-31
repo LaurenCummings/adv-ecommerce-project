0e977f542ff78af3d96fe13661decfd69a669970
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-31 18:55:30 -0500

    added images to categoryItme

--- frontend/src/components/CategoryItem.jsx
+++ frontend/src/components/CategoryItem.jsx
@@ -6,7 +6,12 @@ function CategoryItem( { category } ) {
         <Link>
             <div>
                 <div />
-                <img />
+                <img 
+                    src={category.imageUrl}
+                    alt={category.name}
+                    className="w-full h-full object-cover transition-transform duration-500 ease-out group-hover:scale-110"
+                    loading="lazy"
+                />
                 <div>
                     <h3>{category.name}</h3>
                     <p>Explore {category.name}</p>
