515fd756588c7dd52dcf827fed0f6bc5d7b70ac5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-01-31 17:31:06 -0500

    passed parameters to categoryItem

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,7 +23,10 @@ function HomePage() {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           { categories.map(category => (
-            <CategoryItem />
+            <CategoryItem 
+              category={category}
+              key={category.name}
+            />
           ))}
         </div>
       </div>
