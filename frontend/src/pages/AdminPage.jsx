d240b1228530161750b86dcd379e6d4de5934a73
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-01 03:21:39 -0500

    updated entries in tabs array for products and analytics

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,7 +1,7 @@
 const tabs = [
     { id: "create", label: "Create Product", icon: PlusCircle },
-    { id: "create", label: "Create Product", icon: PlusCircle },
-    { id: "create", label: "Create Product", icon: PlusCircle },
+    { id: "products", label: "Products", icon: ShoppingBasket },
+    { id: "analytics", label: "Analytics", icon: BarChart },
 ];
 
 function AdminPage() {
