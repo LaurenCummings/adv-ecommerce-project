80ddba887a8aa42f9779264cdc91aebca079d249
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-01 03:20:47 -0500

    duplicated entry in tabs array for other tabs

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,6 +1,8 @@
 const tabs = [
     { id: "create", label: "Create Product", icon: PlusCircle },
-]
+    { id: "create", label: "Create Product", icon: PlusCircle },
+    { id: "create", label: "Create Product", icon: PlusCircle },
+];
 
 function AdminPage() {
   return (
