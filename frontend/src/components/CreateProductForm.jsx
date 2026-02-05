13b2f8b82624141cea393ae8bb7e1e240e547626
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-05 02:37:06 -0500

    added categories array to createProductForm component

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { PlusCircle, Upload, Loader } from "lucide-react";
 
+const categories = ["jean", "t-shirt", "shoe", "glasses", "jacket", "suit", "bag"];
+
 function CreateProductForm() {
   return (
     <div>CreateProductForm</div>
