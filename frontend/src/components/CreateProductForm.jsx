a3e89d94630455f8c70d373469d3832b93f4a3e1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-10 00:24:02 -0500

    made select element required

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -84,6 +84,7 @@ function CreateProductForm() {
             value={newProduct.category}
             onChange={(e) => setNewProduct({ ...newProduct, cateogy: e.target.value })}
             className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            required
           >
             <option value="">Select a category</option>
             {categories.map((category) => (
