6376f0e3ad246d29d7a3c97c15adb0f9d0448090
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-10 01:50:50 -0500

    added styling to image upload label

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -101,8 +101,11 @@ function CreateProductForm() {
 
         <div className="mt-1 flex items-center">
           <input type="file" id="image" onChange={handleImageChange} className="sr-only" accept="image/*" />
-          <label>
-            
+          <label
+            htmlFor="image"
+            className="cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-emerald-500"
+          >
+
           </label>
         </div>
       </form>
