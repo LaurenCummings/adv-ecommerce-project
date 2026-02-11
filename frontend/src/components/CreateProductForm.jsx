7167c764174a27e2b9f7a2550bddb772efdc4186
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-11 17:31:15 -0500

    added text underneath loader

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -121,6 +121,7 @@ function CreateProductForm() {
           { loading ? (
             <>
               <Loader className="mr-2 h-5 w-5 animate-spin" aria-hidden="true" />
+              Loading...
             </>
           ) : (
             <>
