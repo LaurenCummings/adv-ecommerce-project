9f85eb89e4eaad9ad9ec2e66a2f58a2841b9b384
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-15 23:50:16 -0500

    fixed typo with select element

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -103,7 +103,7 @@ function CreateProductForm() {
             id="category"
             name="category"
             value={newProduct.category}
-            onChange={(e) => setNewProduct({ ...newProduct, cateogy: e.target.value })}
+            onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
             className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
             required
           >
