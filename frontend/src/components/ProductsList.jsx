fa4c695cf9adabe44ddee9d47de8e065cdc8c957
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-17 01:15:57 -0500

    imported useProductStore into ProductsList

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { Trash, Star } from "lucide-react";
+import { useProductStore } from "../stores/useProductStore";
 
 function ProductsList() {
   return (
