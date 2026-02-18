d6369a64dd8aa0cd6cc1d3b51f6c81af2e905dea
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-17 23:53:29 -0500

    imported useEffect into ProductsList

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { Trash, Star } from "lucide-react";
 import { useProductStore } from "../stores/useProductStore";
+import { useEffect } from "react";
 
 function ProductsList() {
   const { deleteProduct, toggleFeaturedProduct, products } = useProductStore();
