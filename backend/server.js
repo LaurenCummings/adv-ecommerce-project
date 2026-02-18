72afe2774bef16dcb66263853aa8d4ee59806fc2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-18 00:21:44 -0500

    added cors middleware to server file

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes);
