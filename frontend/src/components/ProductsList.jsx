bd33ca7cfd79a43b3b82e170c9c372d96d12d853
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-19 17:58:34 -0500

    added styling to table column

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -17,8 +17,11 @@ function ProductsList() {
       <table className="min-w-full divide-y divide-gray-700">
         <thead className="bg-gray-700">
           <tr>
-            <th>
-              
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+
             </th>
           </tr>
 
