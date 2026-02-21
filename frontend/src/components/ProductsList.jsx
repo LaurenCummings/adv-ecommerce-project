f0b1614cac3fd1c4c276d1388b85062d42e8fff9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-20 23:47:25 -0500

    added a column to the table for actions

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -47,6 +47,12 @@ function ProductsList() {
             >
               Featured
             </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider"
+            >
+              Actions
+            </th>
           </tr>
 
         </thead>
