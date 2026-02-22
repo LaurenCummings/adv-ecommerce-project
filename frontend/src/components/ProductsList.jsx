dc5309a17072602c8752baa9f0372b00206adc40
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-21 23:57:47 -0500

    added category text

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -78,7 +78,7 @@ function ProductsList() {
                 <div className="text-sm text-gray-300">${product.price.toFixed(2)}</div>
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
-                <div></div>
+                <div>{product.category}</div>
               </td>
             </tr>
           ))}
