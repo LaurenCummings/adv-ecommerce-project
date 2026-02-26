0743bf6d775e2ba067522051bc33e396bb3f2a40
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-26 04:10:07 -0500

    deleted conditional for category page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
           <Route path="/signup" element={ !user ? <SignUpPage /> : <Navigate to="/" /> } />
           <Route path="/login" element={ !user ? <LoginPage /> : <Navigate to="/" /> } />
           <Route path="/secret-dashboard" element={ user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
-          <Route path="/category/:category" element={ user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
+          <Route path="/category/:category" element={ <AdminPage /> } />
         </Routes>      
       </div>
       <Toaster />
