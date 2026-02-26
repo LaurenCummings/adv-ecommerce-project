55af2829384f73e7a3cd69ac7180a924a0f97e1a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-26 04:08:58 -0500

    added route for category page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,6 +36,7 @@ function App() {
           <Route path="/signup" element={ !user ? <SignUpPage /> : <Navigate to="/" /> } />
           <Route path="/login" element={ !user ? <LoginPage /> : <Navigate to="/" /> } />
           <Route path="/secret-dashboard" element={ user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
+          <Route path="/category/:category" element={ user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
         </Routes>      
       </div>
       <Toaster />
