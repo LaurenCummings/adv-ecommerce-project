bee268777a501f425ee5887111b2c0e50125c047
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-26 04:13:32 -0500

    updated route for categoryPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
           <Route path="/signup" element={ !user ? <SignUpPage /> : <Navigate to="/" /> } />
           <Route path="/login" element={ !user ? <LoginPage /> : <Navigate to="/" /> } />
           <Route path="/secret-dashboard" element={ user?.role === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
-          <Route path="/category/:category" element={ <AdminPage /> } />
+          <Route path="/category/:category" element={ <CategoryPage /> } />
         </Routes>      
       </div>
       <Toaster />
