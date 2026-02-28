68559eb4ccbe6b1a6844dc42787c43d5aae8e8fa
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-28 00:27:21 -0500

    added fetchProductsByCategory function to categoryPage

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,6 +1,8 @@
 import { useProductStore } from "../stores/useProductStore"
 
 function CategoryPage() {
+  const { fetchProductsByCategory } = useProductStore();
+
   return (
     <div>CategoryPage</div>
   )
