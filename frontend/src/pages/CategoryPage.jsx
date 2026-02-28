198a0c55f9b73052b4dc8d883a9243e86dbacf4f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2026-02-28 02:50:49 -0500

    imported useParams into categoryPage

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,5 +1,6 @@
 import { useProductStore } from "../stores/useProductStore";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function CategoryPage() {
   const { fetchProductsByCategory, products } = useProductStore();
